a6831cd0fb675d342c02c04a7220f2c92849467f
Olivia R Hardwidge <oh72183@srcs.loc>
2017-12-19 11:52:59 -0800

    olvia

--- final_challenge.js
+++ final_challenge.js
@@ -1,17 +1,36 @@
 
 function checkPrime(number) {
-    var result = "";
-    for(i = 2; i <= number; i++) {
-        if(number == 2) {
-            result = "PRIME";
+
+    if (number % 1 != 0 || number <= 0) {
+            return "not prime" ;
         }
-        else if(number % i == 0 || number <= 1 || number % 1 !== 0) {
-            result = "NOT PRIME";
-            return result;
+        else if(number > 4) {
+            for (var i = 2; i<= number / 2; i++) {
+                if (number % 1 == 0 || number % i == 0) {
+                    return "Not Prme"
+                }
+
+            }
+            return "Prime";
         }
         else {
-            result = "PRIME";
+            return "PRIME";
         }
     }
-    return result;
+
+function table (n){
+    var rows = n;
+    var cells = n;
+    document.write("<table border= 1>");
+    for (i = 1; i<= rows; i++){
+        document.write("<tr>");
+        for(j =1; j <= cells; j++) {
+            document.write("<td>", i * j, "<td>");
+
+        }
+        document.write("</tr>")
+    }
+    reutrn("</table>")
 }
+
+f
